3a6ea141bfe969db375f61f357850aad19c161dc
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-07-17 14:26:08 -0500

    Create WarfareRepair.java

---
+++ src/main/java/net/countercraft/movecraft/warfare/utils/WarfareRepair.java
@@ -0,0 +1,115 @@
+package net.countercraft.movecraft.warfare.utils;
+
+import com.sk89q.worldedit.MaxChangedBlocksException;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.blocks.BaseBlock;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.extent.Extent;
+import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
+import com.sk89q.worldedit.function.mask.BlockMask;
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Polygonal2DRegion;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.world.registry.WorldData;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedPolygonalRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import net.countercraft.movecraft.config.Settings;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WarfareRepair {
+    private static WarfareRepair instance;
+    private final Plugin plugin;
+
+    public WarfareRepair(Plugin plugin) {
+        this.plugin = plugin;
+        instance = this;
+    }
+
+    public static WarfareRepair getInstance() {
+        return instance;
+    }
+
+    public boolean saveRegionRepairState(World world, ProtectedRegion region) {
+        File saveDirectory = new File(plugin.getDataFolder(), "AssaultSnapshots");
+        com.sk89q.worldedit.world.World weWorld = new BukkitWorld(world);
+        WorldData worldData = weWorld.getWorldData();
+        Vector weMinPos = region.getMinimumPoint();
+        Vector weMaxPos = region.getMaximumPoint();
+        if (!saveDirectory.exists()) {
+            saveDirectory.mkdirs();
+        }
+        Set<BaseBlock> baseBlockSet = new HashSet<>();
+        Region weRegion = null;
+        if (region instanceof ProtectedCuboidRegion) {
+            weRegion = new CuboidRegion(weMinPos, weMaxPos);
+        } else if (region instanceof ProtectedPolygonalRegion) {
+            ProtectedPolygonalRegion polyReg = (ProtectedPolygonalRegion) region;
+            weRegion = new Polygonal2DRegion(weWorld, polyReg.getPoints(), polyReg.getMinimumPoint().getBlockY(), polyReg.getMaximumPoint().getBlockY());
+        }
+
+
+        File repairStateFile = new File(saveDirectory, region.getId().replaceAll("´\\s+", "_") + ".schematic");
+        for (int x = weMinPos.getBlockX(); x <= weMaxPos.getBlockX(); x++) {
+            for (int y = weMinPos.getBlockY(); y <= weMaxPos.getBlockY(); y++) {
+                for (int z = weMinPos.getBlockZ(); z <= weMaxPos.getBlockZ(); z++) {
+                    Block block = world.getBlockAt(x, y, z);
+                    if (block.getType().equals(Material.AIR)) {
+                        continue;
+                    }
+                    if (Settings.AssaultDestroyableBlocks.contains(block.getTypeId())) {
+                        baseBlockSet.add(new BaseBlock(block.getTypeId(), block.getData()));
+                    }
+                }
+            }
+        }
+        try {
+
+            BlockArrayClipboard clipboard = new BlockArrayClipboard(weRegion);
+            Extent source = WorldEdit.getInstance().getEditSessionFactory().getEditSession(weWorld, -1);
+            Extent destination = clipboard;
+            ForwardExtentCopy copy = new ForwardExtentCopy(source, weRegion, clipboard.getOrigin(), destination, weMinPos);
+            BlockMask mask = new BlockMask(source, baseBlockSet);
+            copy.setSourceMask(mask);
+            Operations.completeLegacy(copy);
+            ClipboardWriter writer = ClipboardFormat.SCHEMATIC.getWriter(new FileOutputStream(repairStateFile, false));
+            writer.write(clipboard, worldData);
+            writer.close();
+            return true;
+
+        } catch (MaxChangedBlocksException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Clipboard loadRegionRepairStateClipboard(String s, World world) {
+        File dataDirectory = new File(plugin.getDataFolder(), "AssaultSnapshots");
+        File file = new File(dataDirectory, s + ".schematic"); // The schematic file
+        com.sk89q.worldedit.world.World weWorld = new BukkitWorld(world);
+        WorldData worldData = weWorld.getWorldData();
+        try {
+            return ClipboardFormat.SCHEMATIC.getReader(new FileInputStream(file)).read(worldData);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
