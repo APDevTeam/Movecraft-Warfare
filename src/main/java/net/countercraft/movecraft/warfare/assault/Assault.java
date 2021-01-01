b707233bdccd92aab55c7e71d16c1a66d22812e1
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-01-01 17:27:44 -0600

    I hate assault code
    This is a steaming pile of garbage

--- src/main/java/net/countercraft/movecraft/warfare/assault/Assault.java
+++ src/main/java/net/countercraft/movecraft/warfare/assault/Assault.java
@@ -1,8 +1,12 @@
 package net.countercraft.movecraft.warfare.assault;
 
 import com.sk89q.worldedit.Vector;
-import org.bukkit.World;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import net.countercraft.movecraft.localisation.I18nSupport;
+import org.bukkit.*;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -11,17 +15,17 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Represents an assault
  */
 public class Assault {
-    private final String regionName;
+    private final @NotNull ProtectedRegion region;
     private final UUID starterUUID;
     private final long startTime;
     private long damages;
     private final long maxDamages;
     private final World world;
     private final Vector minPos, maxPos;
-    private AtomicBoolean running = new AtomicBoolean(true);
+    private final AtomicBoolean running = new AtomicBoolean(true);
 
-    public Assault(String regionName, Player starter, World world, long startTime, long maxDamages, Vector minPos, Vector maxPos) {
-        this.regionName = regionName;
+    public Assault(@NotNull ProtectedRegion region, Player starter, World world, long startTime, long maxDamages, Vector minPos, Vector maxPos) {
+        this.region = region;
         starterUUID = starter.getUniqueId();
         this.world = world;
         this.startTime = startTime;
@@ -40,6 +44,11 @@ public class Assault {
 
     public World getWorld() {
         return world;
+    }
+
+    @NotNull
+    public ProtectedRegion getRegion() {
+        return region;
     }
 
     public long getStartTime() {
@@ -64,10 +73,74 @@ public class Assault {
 
 
     public String getRegionName() {
-        return regionName;
+        return region.getId();
     }
 
     public AtomicBoolean getRunning() {
         return running;
+    }
+
+
+    public boolean makeBeacon() {
+        //first, find a position for the repair beacon
+        int beaconX = minPos.getBlockX();
+        int beaconZ = minPos.getBlockZ();
+        int beaconY;
+        for(beaconY = 255; beaconY > 0; beaconY--) {
+            if(world.getBlockAt(beaconX, beaconY, beaconZ).getType().isOccluding()) {
+                beaconY++;
+                break;
+            }
+        }
+        if(beaconY > 250)
+            return false;
+
+        int x, y, z;
+        for (x = beaconX; x < beaconX + 5; x++)
+            for (z = beaconZ; z < beaconZ + 5; z++)
+                if (!world.isChunkLoaded(x >> 4, z >> z))
+                    world.loadChunk(x >> 4, z >> 4);
+        boolean empty = false;
+        while (!empty && beaconY < 250) {
+            empty = true;
+            beaconY++;
+            for (x = beaconX; x < beaconX + 5; x++) {
+                for (y = beaconY; y < beaconY + 4; y++) {
+                    for (z = beaconZ; z < beaconZ + 5; z++) {
+                        if (!world.getBlockAt(x, y, z).isEmpty())
+                            empty = false;
+                    }
+                }
+            }
+        }
+
+        //now make the beacon
+        y = beaconY;
+        for (x = beaconX + 1; x < beaconX + 4; x++)
+            for (z = beaconZ + 1; z < beaconZ + 4; z++)
+                world.getBlockAt(x, y, z).setType(Material.BEDROCK);
+        y = beaconY + 1;
+        for (x = beaconX; x < beaconX + 5; x++)
+            for (z = beaconZ; z < beaconZ + 5; z++)
+                if (x == beaconX || z == beaconZ || x == beaconX + 4 || z == beaconZ + 4)
+                    world.getBlockAt(x, y, z).setType(Material.BEDROCK);
+                else
+                    world.getBlockAt(x, y, z).setType(Material.IRON_BLOCK);
+        y = beaconY + 2;
+        for (x = beaconX + 1; x < beaconX + 4; x++)
+            for (z = beaconZ + 1; z < beaconZ + 4; z++)
+                world.getBlockAt(x, y, z).setType(Material.BEDROCK);
+
+        world.getBlockAt(beaconX + 2, beaconY + 2, beaconZ + 2).setType(Material.BEACON);
+        world.getBlockAt(beaconX + 2, beaconY + 3, beaconZ + 2).setType(Material.BEDROCK);
+        // finally the sign on the beacon
+        world.getBlockAt(beaconX + 2, beaconY + 3, beaconZ + 1).setType(Material.WALL_SIGN);
+        Sign s = (Sign) world.getBlockAt(beaconX + 2, beaconY + 3, beaconZ + 1).getState();
+        s.setLine(0, ChatColor.RED + I18nSupport.getInternationalisedString("Region Damaged"));
+        s.setLine(1, I18nSupport.getInternationalisedString("Region Name") + ":" + getRegionName());
+        s.setLine(2, I18nSupport.getInternationalisedString("Damages") + ":" + getMaxDamages());
+        s.setLine(3, I18nSupport.getInternationalisedString("Region Owner") + ":" + AssaultUtils.getRegionOwnerList(region));
+        s.update();
+        return true;
     }
 }
--- src/main/java/net/countercraft/movecraft/warfare/assault/AssaultUtils.java
+++ src/main/java/net/countercraft/movecraft/warfare/assault/AssaultUtils.java
@@ -1,12 +1,20 @@
 package net.countercraft.movecraft.warfare.assault;
 
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import net.countercraft.movecraft.Movecraft;
+import net.countercraft.movecraft.warfare.MovecraftWarfare;
 import net.countercraft.movecraft.warfare.config.Config;
+import net.countercraft.movecraft.warfare.siege.Siege;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -23,7 +31,7 @@ public class AssaultUtils {
         return numOnline >= Config.AssaultRequiredDefendersOnline;
     }
 
-    private static int ownersOnline(ProtectedRegion region) {
+    private static int ownersOnline(@NotNull ProtectedRegion region) {
         int numOnline = 0;
         Set<UUID> owners = region.getOwners().getUniqueIds();
         for(UUID playerID : owners) {
@@ -33,7 +41,7 @@ public class AssaultUtils {
         return numOnline;
     }
 
-    private static int membersOnline(ProtectedRegion region) {
+    private static int membersOnline(@NotNull ProtectedRegion region) {
         int numOnline = 0;
         Set<UUID> members = region.getMembers().getUniqueIds();
         for(UUID playerID : members) {
@@ -55,33 +63,105 @@ public class AssaultUtils {
         return getOwnerBalance(tRegion) + getMemberBalance(tRegion);
     }
 
-    private static double getOwnerBalance(ProtectedRegion tRegion) {
-        HashSet<UUID> players = new HashSet<>();
-        players.addAll(tRegion.getOwners().getUniqueIds());
+    private static double getOwnerBalance(@NotNull ProtectedRegion tRegion) {
+        HashSet<UUID> players = new HashSet<>(tRegion.getOwners().getUniqueIds());
         double total = 0.0;
         for (UUID playerID : players) {
             OfflinePlayer offP = Bukkit.getOfflinePlayer(playerID);
             if (offP.getName() != null)
-                if (Movecraft.getInstance().getEconomy().getBalance(offP) > Config.AssaultMaxBalance)
-                    total += Config.AssaultMaxBalance;
-                else
-                    total += Movecraft.getInstance().getEconomy().getBalance(offP);
+                total += Math.min(Movecraft.getInstance().getEconomy().getBalance(offP), Config.AssaultMaxBalance);
         }
         return total * (Config.AssaultOwnerWeightPercent / 100.0);
     }
 
-    private static double getMemberBalance(ProtectedRegion tRegion) {
-        HashSet<UUID> players = new HashSet<>();
-        players.addAll(tRegion.getMembers().getUniqueIds());
+    private static double getMemberBalance(@NotNull ProtectedRegion tRegion) {
+        HashSet<UUID> players = new HashSet<>(tRegion.getMembers().getUniqueIds());
         double total = 0.0;
         for (UUID playerID : players) {
             OfflinePlayer offP = Bukkit.getOfflinePlayer(playerID);
             if (offP.getName() != null)
-                if (Movecraft.getInstance().getEconomy().getBalance(offP) > Config.AssaultMaxBalance)
-                    total += Config.AssaultMaxBalance;
-                else
-                    total += Movecraft.getInstance().getEconomy().getBalance(offP);
+                total += Math.min(Movecraft.getInstance().getEconomy().getBalance(offP), Config.AssaultMaxBalance);
         }
         return total * (Config.AssaultMemberWeightPercent / 100.0);
+    }
+
+    public static boolean ownsRegions(Player p) {
+        LocalPlayer lp = Movecraft.getInstance().getWorldGuardPlugin().wrapPlayer(p);
+        Map<String, ProtectedRegion> allRegions = Movecraft.getInstance().getWorldGuardPlugin().getRegionManager(p.getWorld()).getRegions();
+        for (ProtectedRegion iRegion : allRegions.values()) {
+            if (iRegion.isOwner(lp) && iRegion.getFlag(DefaultFlag.TNT) == StateFlag.State.DENY) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isMember(Player p, @NotNull ProtectedRegion r) {
+        LocalPlayer lp = Movecraft.getInstance().getWorldGuardPlugin().wrapPlayer(p);
+        return r.isMember(lp) || r.isOwner(lp);
+    }
+
+    public static boolean canAssault(@NotNull ProtectedRegion region) {
+        // a region can only be assaulted if it disables TNT, this is to prevent child regions or sub regions from being assaulted
+        // regions with no owners can not be assaulted
+        if (region.getFlag(DefaultFlag.TNT) != StateFlag.State.DENY || region.getOwners().size() == 0)
+            return false;
+
+        for (Siege siege : MovecraftWarfare.getInstance().getSiegeManager().getSieges()) {
+            // siegable regions can not be assaulted
+            if (region.getId().equalsIgnoreCase(siege.getAttackRegion()) || region.getId().equalsIgnoreCase(siege.getCaptureRegion())) {
+                return false;
+            }
+        }
+
+        // TODO: This is 100% broken, instead we need to use a file to store the last assault data.
+        /*{
+            Assault assault = null;
+            for (Assault tempAssault : MovecraftWarfare.getInstance().getAssaultManager().getAssaults()) {
+                if (tempAssault.getRegion().equals(region)) {
+                    assault = tempAssault;
+                    break;
+                }
+            }
+            if (assault != null) {
+                long startTime = assault.getStartTime();
+                long curtime = System.currentTimeMillis();
+                if (curtime - startTime < Config.AssaultCooldownHours * (60 * 60 * 1000)) {
+                    return false;
+                }
+            }
+        }*/
+        return true;
+    }
+
+    @NotNull
+    public static String getRegionOwnerList(ProtectedRegion tRegion) {
+        StringBuilder output = new StringBuilder();
+        if (tRegion == null)
+            return "";
+        boolean first = true;
+        if (tRegion.getOwners().getUniqueIds().size() > 0) {
+            for (UUID uid : tRegion.getOwners().getUniqueIds()) {
+                if (!first)
+                    output.append(",");
+                else
+                    first = false;
+                OfflinePlayer offP = Bukkit.getOfflinePlayer(uid);
+                if (offP.getName() == null)
+                    output.append(uid.toString());
+                else
+                    output.append(offP.getName());
+            }
+        }
+        if (tRegion.getOwners().getPlayers().size() > 0) {
+            for (String player : tRegion.getOwners().getPlayers()) {
+                if (!first)
+                    output.append(",");
+                else
+                    first = false;
+                output.append(player);
+            }
+        }
+        return output.toString();
     }
 }
