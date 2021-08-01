c32a9bc6cafc42855096317eb7c77e4cb4f6e716
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-08-01 07:59:12 -0500

    Move minute formatting

--- src/main/java/net/countercraft/movecraft/warfare/siege/SiegeTask.java
+++ src/main/java/net/countercraft/movecraft/warfare/siege/SiegeTask.java
@@ -12,18 +12,4 @@ public abstract class SiegeTask extends BukkitRunnable {
 
     @Override
     public abstract void run();
-
-    public String formatMinutes(int seconds) {
-        if (seconds < 60) {
-            return I18nSupport.getInternationalisedString("Siege - Ending Soon");
-        }
-
-        int minutes = seconds / 60;
-        if (minutes == 1) {
-            return I18nSupport.getInternationalisedString("Siege - Ending In 1 Minute");
-        }
-        else {
-            return String.format(I18nSupport.getInternationalisedString("Siege - Ending In X Minutes"), minutes);
-        }
-    }
 }
