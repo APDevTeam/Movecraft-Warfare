43410c425b78be52b1b4c2d5be703ccc5f7193ee
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-02-06 12:01:53 -0600

    Redo Siege

--- src/main/java/net/countercraft/movecraft/warfare/features/siege/Siege.java
+++ src/main/java/net/countercraft/movecraft/warfare/features/siege/Siege.java
@@ -1,8 +1,11 @@
 package net.countercraft.movecraft.warfare.features.siege;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class Siege {
     public enum Stage {
@@ -11,6 +14,8 @@ public class Siege {
 
     @NotNull private final SiegeConfig config;
     @NotNull private final AtomicReference<Stage> stage;
+    @Nullable private LocalDateTime startTime;
+    @Nullable private OfflinePlayer player;
 
     public Siege(@NotNull SiegeConfig config) {
         this.config = config;
@@ -30,6 +35,16 @@ public class Siege {
 
     public void setStage(@NotNull Stage stage) {
         this.stage.set(stage);
+    }
+
+    @Nullable
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    @Nullable
+    public OfflinePlayer getPlayer() {
+        return player;
     }
 
     public String toString() {
