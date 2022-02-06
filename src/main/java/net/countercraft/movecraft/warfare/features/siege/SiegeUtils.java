6df1cb1abdcce43a4966738c898592ad0523b831
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-02-06 10:07:51 -0600

    Move SiegeUtils

--- src/main/java/net/countercraft/movecraft/warfare/features/siege/SiegeUtils.java
+++ src/main/java/net/countercraft/movecraft/warfare/features/siege/SiegeUtils.java
@@ -1,14 +1,14 @@
-package net.countercraft.movecraft.warfare.siege;
+package net.countercraft.movecraft.warfare.features.siege;
 
 import net.countercraft.movecraft.warfare.localisation.I18nSupport;
 
 public class SiegeUtils {
-    public static String formatMinutes(int seconds) {
+    public static String formatMinutes(long seconds) {
         if (seconds < 60) {
             return I18nSupport.getInternationalisedString("Siege - Ending Soon");
         }
 
-        int minutes = seconds / 60;
+        long minutes = seconds / 60;
         if (minutes == 1) {
             return I18nSupport.getInternationalisedString("Siege - Ending In 1 Minute");
         } else {
