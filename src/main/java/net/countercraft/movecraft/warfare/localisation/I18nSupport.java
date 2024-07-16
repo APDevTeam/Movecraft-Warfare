17547fa4fcb1e1a7d16fa792b547b0c81a4181aa
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-07-15 19:06:02 -0500

    Deprecate string localization

--- src/main/java/net/countercraft/movecraft/warfare/localisation/I18nSupport.java
+++ src/main/java/net/countercraft/movecraft/warfare/localisation/I18nSupport.java
@@ -2,16 +2,20 @@ package net.countercraft.movecraft.warfare.localisation;
 
 import net.countercraft.movecraft.warfare.MovecraftWarfare;
 import net.countercraft.movecraft.warfare.config.Config;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.util.Properties;
 import java.util.logging.Level;
 
 public class I18nSupport {
-    private static Properties langFile;
+    private static Properties languageFile;
 
     public static void init() {
-        langFile = new Properties();
+        languageFile = new Properties();
 
         File langDirectory = new File(MovecraftWarfare.getInstance().getDataFolder().getAbsolutePath() + "/localisation");
         if (!langDirectory.exists()) {
@@ -19,7 +23,6 @@ public class I18nSupport {
         }
 
         InputStream stream = null;
-
         try {
             stream = new FileInputStream(langDirectory.getAbsolutePath()+"/mcwlang_" + Config.Locale + ".properties");
         } catch (FileNotFoundException e) {
@@ -32,18 +35,28 @@ public class I18nSupport {
         }
 
         try {
-            langFile.load(stream);
+            languageFile.load(stream);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static String getInternationalisedString(String key) {
-        String ret = langFile.getProperty(key);
+    private static String get(String key) {
+        String ret = languageFile.getProperty(key);
         if (ret != null) {
             return ret;
         } else {
             return key;
         }
+    }
+
+    @Deprecated(forRemoval = true)
+    public static String getInternationalisedString(String key) {
+        return get(key);
+    }
+
+    @Contract("_ -> new")
+    public static @NotNull TextComponent getInternationalisedComponent(String key){
+        return Component.text(get(key));
     }
 }
